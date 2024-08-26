117885da3f0947bf8bb1f5bde6b4b8615f3d4a0c
X1vova1X <128622578+X1vova1X@users.noreply.github.com>
2024-08-26 16:17:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Use express.json() to parse JSON request bodies
+app.use(express.json());
+
+// In-memory database
+let database = {};
+
+// POST endpoint to add a new value
+app.post('/values', (req, res) => {
+  const { value } = req.body;
+  const { id } = req.body;
+  const id2 = String({ id });
+  database[id] = value;
+  res.status(201).json({ id2 });
+});
+
+// GET endpoint to retrieve all values
+app.get('/values', (req, res) => {
+  res.json(database);
+});
+
+app.get('/db/clear', (req, res) => {
+  database = {};
+  res.send("DB cleared.")
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
